2eabdab54e7db18042e08ad01e3d1cbe3f5b314e
Keith Smith <32456218+afewvowels@users.noreply.github.com>
2017-10-11 12:07:32 -0400

    Initial Commit

---
+++ SWFT-PRG-02-18-Energy-Drink-Consumption/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SWFT-PRG-02-18-Energy-Drink-Consumption
+//
+//  Created by Keith Smith on 10/11/17.
+//  Copyright © 2017 Keith Smith. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
